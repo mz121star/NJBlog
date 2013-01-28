83eef926b449ee3e28375f31540309577e1cbcc9
Sailor.Zhang <z@zhangxiangnan.cn>
2013-01-28 17:10:27 +0800

    rollback db.

--- models/db.js
+++ models/db.js
@@ -13,27 +13,5 @@ mongoose.connect(config.connectionstring);
 //mongoose.connect(config.host);
 module.exports = function (name, model, extendMethods) {
     var schema = mongoose.Schema(model);
-    schema.statics.getById = function (id, cb) {
-        this.find({_id:id}, cb);
-    };
-    extendSchemaMethods(extendMethods, schema);
     return mongoose.model(name, schema);
-}
-
-var extendSchemaMethods = function (extendMethods, schema) {
-    extendMethods = extendMethods || {};
-    var statics = extendMethods.statics;
-    var methods = extendMethods.methods;
-    if (statics) {
-        for (var name in statics) {
-            if(statics.hasOwnProperty(name))
-              schema.statics[name] = statics[name];
-        }
-    }
-    if (methods) {
-        for (name in methods) {
-            if(methods.hasOwnProperty(name))
-              schema.methods[name] = methods[name];
-        }
-    }
 };
