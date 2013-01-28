dc0ed14705c07799b10023ddd6cc757ac324f1f7
mz121star <mz121star@gmail.com>
2013-01-28 15:18:47 +0800

    test EVN

--- test/app.js
+++ test/app.js
@@ -1,5 +1,21 @@
 var should = require('should');
+var app = require('../app');
+
 describe('UT test', function () {
+    before(function (done) {
+        app.listen(0, done);
+    });
+    after(function () {
+        app.close();
+    });
+
+    it('should / status 200', function (done) {
+        app.request().get('/').end(function (res) {
+            res.should.status(200);
+            done();
+        });
+    });
+
     it('test can run', function(){
         "test ut".should.include('ut');
     })
