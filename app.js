a2dbbb677dee9fb08c0e4fb83d4d09020950ecdb
mz121star <mz121star@gmail.com>
2013-01-29 15:10:13 +0800

    modified app.js " view engine =html"

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set('view engine', 'html');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
