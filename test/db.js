618d07a8120d31b96bd7f11aad15b07632bc983b
mz121star <mz121star@gmail.com>
2013-01-30 13:09:31 +0800

    add browser support

--- test/db.js
+++ test/db.js
@@ -1,12 +1,17 @@
-var should = require('should');
-var models = require('./../models');
+//var should = require('should');
+//var models = require('./../models');
+"use strict";
+
+var exports = (typeof window === "undefined") ? module.exports : window;
+exports.setupTests=function(models){
 
 describe('DateBase Connection test', function () {
 
     it('db connected', function(){
         models.Blog.find({}, function(error,model){
-            model.should.be.true
+            model.should.equal(true);
         });
     })
 
 });
+}
