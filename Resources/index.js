886af979f184af47e94ae086e5cb2befcf96edf3
mz121star <mz121star@gmail.com>
2013-01-31 10:15:45 +0800

    add Localization  support

---
+++ Resources/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var zhCN=require("./zh-CN");
+
+(function () {
+    var root = this;
+
+    var language={
+
+    }
+    /*export language to window or server */
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = language;
+        }
+        exports.language = language;
+    } else if (typeof define === 'function' && define.amd) {
+        // Register as a named module with AMD.
+        define(function () {
+            return language;
+        });
+    } else {
+        root['language'] = language;
+    }
+}).call(this);
