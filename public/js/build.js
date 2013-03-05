4b159e614fe9fbc15425389b6185f034300a9de2
mz121star <miaozhuang@0121>
2013-03-06 00:02:50 +0800

    r.js combine files

---
+++ public/js/build.js
@@ -0,0 +1,27 @@
+({
+    baseUrl: ".",
+    paths: {
+        jquery: '../lib/jquery/jquery-1.8.2.min',
+        bootstrap: '../lib/bootstrap/js/bootstrap',
+        underscore: '../lib/underscore/underscore',
+        angular: '../lib/angular/angular',
+        angularResource: '../lib/angular/angular-resource',
+        text: '../lib/require/text',
+        i18n:'../lib/require/i18n',
+        modernizer:'../lib/modernizr',
+        html5shiv:'../lib/html5shiv',
+        mcore:'../lib/mcore.min',
+        fullscreen:'../lib/fullscreen'
+        //res:'../resources/nls/res'
+
+    },
+    shim: {
+        'angular' : {'exports' : 'angular'},
+        'angular-resource' : {deps:['angular']},
+        'bootstrap': {deps:['jquery']},
+        'underscore': {exports: '_'}
+    },
+
+    name: "main",
+    out: "main-built.js"
+})
